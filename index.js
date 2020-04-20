bd5c789b33ca99cdff203534cf0ef5ddea8f78d6
Donald Lieu <dclieu@ucdavis.edu>
2020-04-20 13:03:59 -0700

    fixed a few indentation errors and more comments

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ async function handleRequest(request) {
 
 	// Get page
 	let test = await fetch(urls[index]);
-	// Make a few modifications
+
+	// Modify request before passing to user
 	test = new HTMLRewriter().on('title', new ElementHandler(index, isNew)).transform(test);
 	test = new HTMLRewriter().on('h1#title', new ElementHandler(index, isNew)).transform(test);
 	test = new HTMLRewriter().on('p#description', new ElementHandler(index, isNew)).transform(test);
@@ -40,6 +41,7 @@ async function handleRequest(request) {
 }
 
 class ElementHandler {
+	
 	constructor(index, isNew) {
 			this.index = index;
 			this.isNew = isNew;
@@ -47,15 +49,16 @@ class ElementHandler {
 
 	element(element) {
 		if (element.tagName == 'title') {
-				element.setInnerContent('Coin Flipper');
+			element.setInnerContent('Coin Flipper');
 		} else if (element.tagName == 'h1' && element.getAttribute('id') == 'title') {
-				element.setInnerContent(this.index == 1 ? 'Heads!' : "Tails!");
+			element.setInnerContent(this.index == 1 ? 'Heads!' : "Tails!");
 		} else if (element.tagName == 'p' && element.getAttribute('id') == 'description') {
-				element.setInnerContent(this.isNew ? `Thanks for taking a look at my project! Have a cookie!` : `Fun fact: a universe might have diverged from the moment you flipped the coin... you should do it again!`);
+			element.setInnerContent(this.isNew ? `Thanks for taking a look at my project! Have a cookie!` : `Fun fact: a universe might have diverged from the moment you flipped the coin... you should do it again!`);
 		} else if (element.tagName == 'a' && element.getAttribute('id') == 'url') {
-				element.setInnerContent('Click here to flip again');
-				element.setAttribute('href', 'https://cloudflare-app.blaxe.workers.dev/');
-				element.setAttribute('onclick', `document.cookie="coinIndex=2; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/"`);
+			// Set cookie to signal generation of a new value. 
+			element.setInnerContent('Click here to flip again');
+			element.setAttribute('href', 'https://cloudflare-app.blaxe.workers.dev/');
+			element.setAttribute('onclick', `document.cookie="coinIndex=2; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/"`);
 		}
 	}
 
